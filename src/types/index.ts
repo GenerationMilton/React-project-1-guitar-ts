cc4d1ee8b3f324b58e038a81cc0a634eb1cd9c35
Milton Munoz <livemilton@gmail.com>
2025-11-23 21:35:08 -0500

    Angular with ts: :sparkles: Introduction to Ts - Inherit and extend a type

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,12 +7,7 @@ export type Guitar = {
 }
 
 
-export type CartItem = {
-    id : number;
-    name: string
-    image: string;
-    description: string;
-    price: number;
+export type CartItem = Guitar & {
     quantity: number
 }
 
